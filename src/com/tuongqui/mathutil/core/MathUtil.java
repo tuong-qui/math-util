553afe1855a2f15f2efb70dff5ed65f6d5b40f70
tuong-qui <tuongqui2005@gmail.com>
2025-08-26 16:35:46 +0700

    refactor the main code and green 4ever always

--- src/com/tuongqui/mathutil/core/MathUtil.java
+++ src/com/tuongqui/mathutil/core/MathUtil.java
@@ -17,16 +17,31 @@ public class MathUtil {
     // hàm thư viện dùng chung cho ai đó ko cân lưu lại trạng thái/ giá trị --> chọn thiết kế STATIC
     
     // hàm giai thừa 
-    public static long getFactorial (int n) {
-        if (n < 0 || n > 20)
-            throw new IllegalArgumentException("Invalid argument. N must be between 0 ... 20.");
+//    public static long getFactorial (int n) {
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 ... 20.");
+//        if ( n == 0 || n == 1)
+//            return 1; 
+//        long product = 1; // tích nhân dồn
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//        return product;
+//                
+//    }
+    
+    // RECURSION TEST
+    // Đệ quy ;à hiện tượng gọi lại chính nó với 1 quy mô khác
+    // ví dụ: búp bê Nga, giống nhau và lồng nhau
+    // 
+    
+        public static long getFactorial (int n) {
+        
         if ( n == 0 || n == 1)
             return 1; 
-        long product = 1; // tích nhân dồn
-        for (int i = 2; i <= n; i++)
-            product *= i;
-        return product;
+        
+        return n * getFactorial(n - 1);
                 
     }
+
     
 }
